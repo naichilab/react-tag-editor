7b1e450f117898b936070d0c637633b05ebc94a3
naichilab <naichilab@live.jp>
2021-05-25 01:33:42 +0900

    TagEditorとりあえず表示

---
+++ src/TagEditor.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import './TagEditor.css';
+
+const TagEditor: FC = () => (
+  <div className="TagEditor">
+    <h1>this is tag editor.</h1>
+  </div>
+);
+
+export default TagEditor;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TagEditor from './TagEditor';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TagEditor />
   </React.StrictMode>,
   document.getElementById('root'),
 );
