50a8b189ed453695595f3fa6db569fcb95299160
naichilab <naichilab@live.jp>
2021-05-27 02:49:57 +0900

    見た目変更

--- src/components/TagEditor/TagName.tsx
+++ src/components/TagEditor/TagName.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
 import './TagName.css';
 import lockimg from './lock.png';
-import close from './close.png';
 
 type Props = {
   lock: boolean;
@@ -11,9 +10,9 @@ type Props = {
 const TagName: FC<Props> = ({ name, lock }: Props) => (
   <div className="TagName">
     <div className="TagName-Contents">
-      {lock && <img className="TagName-LockImg" src={lockimg} alt="" />}
       <div className="TagName-Text">{name}</div>
-      <img className="TagName-Delete" src={close} alt="" />
+      {lock && <img className="TagName-LockImg" src={lockimg} alt="" />}
+      {!lock && <div className="TagName-Delete" />}
     </div>
   </div>
 );
